98e46da9f3e689f1432d922b054dc0ab6b07f8fd
doanmade <43007082+doanmade@users.noreply.github.com>
2019-07-11 15:53:25 -0500

    MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,36 @@
            https://api.github.com/users/<your name>
 */
 
+const userContainer = document.querySelector(".cards");
+axios
+  .get("https://api.github.com/users/doanmade")
+
+  .then(results => {
+    userContainer.appendChild(userCard(results.data));
+    axios
+      .get(results.data.followers_url)
+      .then(result => {
+        const followersAuto = result.data.map(follower => follower.login);
+
+        followersAuto.forEach(follower => {
+          axios
+            .get(`https://api.github.com/users/${follower}`)
+            .then(result => {
+              userContainer.appendChild(userCard(result.data));
+            })
+            .catch(error => {
+              console.log("Server error", error);
+            });
+        });
+      })
+      .catch(error => {
+        console.log("I have no followers");
+      });
+  })
+  .catch(error => {
+    console.log("Error", error);
+  });
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -24,8 +54,78 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell"
+];
 
+followersArray.forEach(follower => {
+  axios
+    .get(`https://api.github.com/users/${follower}`)
+    .then(response => {
+      console.log(response);
+      userContainer.appendChild(userCard(response.data));
+    })
+    .catch(err => {
+      console.log("Can't find request");
+    });
+});
+
+function userCard(userObject) {
+  // create the elements
+
+  const cardElement = document.createElement("div");
+  const userImage = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const name = document.createElement("h3");
+  const userName = document.createElement("p");
+  const location = document.createElement("p");
+  const profile = document.createElement("p");
+  const address = document.createElement("a");
+  const followersCount = document.createElement("p");
+  const followingCount = document.createElement("p");
+  const bio = document.createElement("p");
+  // set the styles
+
+  // appending the node tree structure:
+  cardElement.appendChild(userImage);
+  cardElement.appendChild(cardInfo);
+
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(userName);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  cardInfo.appendChild(followersCount);
+  cardInfo.appendChild(followingCount);
+  cardInfo.appendChild(bio);
+
+  profile.appendChild(address);
+
+  // adding classNames and attributes:
+  cardElement.className = "card";
+  userImage.src = userObject.avatar_url;
+  cardInfo.className = "card-info";
+  address.href = userObject.html_url;
+  name.className = "name";
+  userName.className = "username";
+
+  // text content and user information:
+  name.textContent = userObject.name;
+  userName.textContent = userObject.login;
+  location.textContent = `Location: ${userObject.location}`;
+  profile.textContent = "Profile: ";
+  address.textContent = userObject.html_url;
+  followersCount.textContent = userObject.followers;
+  followingCount.textContent = userObject.following;
+  bio.textContent = `Bio: ${userObject.bio}`;
+
+  //Test html structure:
+  console.log(cardElement); // everything looks good.
+  return cardElement;
+}
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
